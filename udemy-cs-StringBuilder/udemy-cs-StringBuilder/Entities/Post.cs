8308d2b5dcff9d1805e1e633e3e60490d3438956
r_gomes <r.gomes@mega.com.br>
2022-03-08 20:08:09 -0300

    Exercício sobre StringBuildes

---
+++ udemy-cs-StringBuilder/udemy-cs-StringBuilder/Entities/Post.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace udemy_cs_StringBuilder.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes.ToString());
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString());
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments: ");
+            foreach(Comment comment in Comments)
+            {
+                sb.AppendLine(comment.Text);
+            }
+            return sb.ToString();
+
+            //sem o stringbuilder
+            /*string txt = Title + Environment.NewLine;
+            txt += $"{Likes} Likes - " + Moment.ToString() + Environment.NewLine;
+            txt += Content + Environment.NewLine;
+            txt += "Comments: " + Environment.NewLine;
+            foreach(Comment comment in Comments)
+            {
+                txt += comment.Text+Environment.NewLine;
+            }
+            return txt;*/
+        }
+    }
+}
---
+++ udemy-cs-StringBuilder/udemy-cs-StringBuilder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using udemy_cs_StringBuilder.Entities;
+
+namespace udemy_cs_StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow that's awesome!");
+            Comment c3 = new Comment("Good night");
+            Comment c4 = new Comment("May the force be with you");
+
+            Post p1 = new Post(DateTime.Parse("20/06/2018 13:05:44"), "Traveling to New Zealand", "I'm going to visit this wonderful country!", 12);
+            Post p2 = new Post(DateTime.Parse("28/07/2018 23:14:19"), "Good night guys", "See you tomorrow", 5);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.Write(p1.ToString());
+            Console.WriteLine();
+            Console.WriteLine(p2.ToString());
+
+
+        }
+    }
+}
